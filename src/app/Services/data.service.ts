002ac81fc8a5d517071c402b746028ac7352f8c5
Dylan Morrow <G00365708@gmit.ie>
2021-03-10 15:24:35 +0000

    lab complete

---
+++ src/app/Services/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://jsonblob.com/api/jsonblob/6612e687-81aa-11eb-8c36-3fad1a683436');
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&APPID=6a66416403ed8e5e6e762cb8c261f303');
+  }
+}
